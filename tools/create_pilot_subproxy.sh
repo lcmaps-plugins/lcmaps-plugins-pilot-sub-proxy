90895039b75e4069ddf6b0ef4436e9523cde9101
Mischa Salle <msalle@nikhef.nl>
2015-07-09 14:17:52 +0200

    Add more options/flexibility to psp creation tool
    Add more command line options to allow more flexible setting of the DN of the
    payload proxy and overriding the input and output filenames (synced with the
    corresponding PUSP creation tool).

--- tools/create_pilot_subproxy.sh
+++ tools/create_pilot_subproxy.sh
@@ -1,46 +1,133 @@
 #!/bin/sh
-BITS=rsa:1024
-HASH=sha256
+#
+# Copyright (c) FOM-Nikhef 2015-
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors:
+# 2015-
+#    Mischa Sall\'e <msalle@nikhef.nl>
+#    NIKHEF Amsterdam, the Netherlands
+#    <grid-mw-security@nikhef.nl>
+#
 
-if [ $# -lt 1 ];then
-    echo "Usage: $(basename $0) <CN value> [payload proxyfile]" >&2
-    exit 1
-fi
-
-# Input proxy
+# cmdline adaptable defaults:
+VERB=0
+PROXY_CN=""
+user=""
+PREFIX="user:"
 X509_USER_PROXY=${X509_USER_PROXY:-/tmp/x509up_u$(id -u)}
+PROXY_FILE=${X509_USER_PROXY}_payload
 
-# Subject of payload proxy
-PROXY_CN="/CN=$1"
+# Other defaults:
+BITS=1024				# RSA keylength
+HASH=sha256				# hash algorithm
 
-# Output filename
-if [ -z "$2" ];then
-    PROXY_FILE=${X509_USER_PROXY}_payload
-else
-    PROXY_FILE=$2
-fi
+# Script name
+prog=$(basename $0)
 
 ########################################################################
+
+# Usage function
+usage()	{
+    echo "Usage: $(basename $0) <options>"
+    echo "Options:"
+    echo " -h             print this help text"
+    echo " -v             be verbose"
+    echo " -c <cn-value>  CN value (prefix+username), added after \"/CN=\""
+    echo " -u <user>      username, added after \"/CN=${PREFIX}\""
+    echo " -P <prefix>    prefix in /CN field, default: \"$PREFIX\""
+    echo " -x <proxy>     pilot proxy filename, default: \"$X509_USER_PROXY\""
+    echo " -p <proxy>     payload proxy filename, default: \"$PROXY_FILE\""
+    exit 0
+}
+
+# Called for invalid options
+illoption() {
+    case $1 in
+        :)  echo "$prog: option \`-$2' requires an argument" >&2 ;;
+        ?)  echo "$prog: invalid option -- $2" >&2 ;;
+    esac
+    echo "Try \`$prog -h' for more information" >&2
+    exit $3
+}
+
+# Verbose log
+verb()	{
+    if [ $VERB -eq 1 ];then
+	echo "$*"
+    fi
+}
+
+# Get command line options
+while getopts ":hvc:u:P:x:p:" i $*;do
+    case $i in
+	h)  usage ;;
+	v)  VERB=1 ;;
+	c)  PROXY_CN=$OPTARG ;;
+	u)  user=$OPTARG ;;
+	P)  PREFIX=$OPTARG ;;
+	x)  X509_USER_PROXY=$OPTARG ;;
+	p)  PROXY_FILE=$OPTARG ;;
+	:)  illoption $i $OPTARG 2 ;;
+	?)  illoption $i $OPTARG 2 ;;
+    esac
+done
+
+# Check mandatory arguments
+if [ -z "$PROXY_CN" ];then
+    if [ -z "$user" ];then
+	echo "$prog: Missing mandatory option -u <user> or -c <proxy-cn>" >&2
+	echo "Try \`$prog -h' for more information" >&2
+	exit 1
+    fi
+
+    # Subject of payload proxy
+    PROXY_CN="${PREFIX}$user"
+fi
 
 # Set nameopts for getting subject from proxy
 NAMEOPTS="esc_2253,esc_ctrl,utf8,dump_nostr,dump_der,sep_multiline,sname"
 
 # New serial number
-SERIAL=`expr $RANDOM \* $RANDOM`
+SERIAL=$(expr $RANDOM \* $RANDOM)
 
-# Different tempfiles
-OPENSSL_CONF=`mktemp openssl.cnf.XXXXXX`
-PROXYREQ=`mktemp proxyrequest.XXXXXX`
-PROXYKEY=`mktemp proxykey.XXXXXX`
-PROXYCERT=`mktemp proxycert.XXXXXX`
-LOGFILE=`mktemp logfile.XXXXXX`
+# Enforce umask
+umask 077
+
+# Different tempfiles: put in separate directory in $TMPDIR or /tmp
+PROXYTMPDIR=$(mktemp -d --tmpdir create_pusp_XXXXXX)
+OPENSSL_CONF=$(mktemp --tmpdir=$PROXYTMPDIR openssl.cnf.XXXXXX)
+PROXYREQ=$(mktemp --tmpdir=$PROXYTMPDIR proxyrequest.XXXXXX)
+PROXYKEY=$(mktemp --tmpdir=$PROXYTMPDIR proxykey.XXXXXX)
+PROXYCERT=$(mktemp --tmpdir=$PROXYTMPDIR proxycert.XXXXXX)
+LOGFILE=$(mktemp --tmpdir=$PROXYTMPDIR logfile.XXXXXX)
 
 cleanup()   {
+    # Don't do a rm -rf for safety 
     for f in "$OPENSSL_CONF" "$PROXYREQ" "$PROXYKEY" "$PROXYCERT" "$LOGFILE";do
 	if [ -n "$f" -a -f "$f" ];then
 	    rm "$f"
 	fi
     done
+    rmdir $PROXYTMPDIR || {
+	echo "Cleanup of $PROXYTMPDIR failed" >&2
+    }
+}
+
+myexit()    {
+    cleanup
+    exit $1
 }
 
 # Create OpenSSL config file on the fly. Need RFC compliant limited proxy with
@@ -65,29 +152,30 @@ SUBJ=$(openssl x509 -in $X509_USER_PROXY -noout -subject -nameopt $NAMEOPTS|\
        sed '1d;s:^ *:/:'|tr -d '\n')
 if [ -z "$SUBJ" ];then
     echo "Getting subject of $X509_USER_PROXY failed" >&2
-    cleanup
-    exit 1
+    myexit 1
 fi
+verb "Got subject \"$SUBJ\""
 
+# Create certificate signing request
+verb "Generating $BITS bits RSA key and request"
 openssl req \
-    -new -nodes -newkey $BITS -subj "${SUBJ}${PROXY_CN}" \
+    -new -nodes -newkey rsa:$BITS -subj "${SUBJ}/CN=${PROXY_CN}" \
     -keyout $PROXYKEY -out $PROXYREQ 2> $LOGFILE || {
-	echo "Creating request failed" >&2
+	echo "Creating request failed, logfile:" >&2
 	cat $LOGFILE >&2
-	cleanup
-	exit 1
+	myexit 1
     }
 
-
+# Sign certificate signing request, creating proxy certificate
+verb "Signing key and request to create proxy cert"
 openssl x509 \
     -req -CAkeyform pem -in $PROXYREQ -out $PROXYCERT \
     -CA $X509_USER_PROXY -CAkey $X509_USER_PROXY \
     -set_serial $SERIAL -days 1 -$HASH \
     -extfile $OPENSSL_CONF 2> $LOGFILE || {
-	echo "Signing request failed" >&2
+	echo "Signing request failed, logfile:" >&2
 	cat $LOGFILE >&2
-	cleanup
-	exit 1
+	myexit 1
     }
 
 # Add new cert and key to proxy file
@@ -110,4 +198,4 @@ done >> $PROXY_FILE
 # Cleanup temp files
 cleanup
 
-echo "Proxy is left in $PROXY_FILE"
+verb "Proxy is left in $PROXY_FILE"
